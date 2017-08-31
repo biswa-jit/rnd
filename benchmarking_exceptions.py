75964a87b5762e3f6bf00dfbf7bf394f09771d8c
Biswajit Sahu <biswajit@spoyl.in>
2017-08-31 13:38:11 +0530

    benchmarking exceptions done

--- benchmarking_exceptions.py
+++ benchmarking_exceptions.py
@@ -1,20 +1,48 @@
-SETUP = 'counter = 0'
+"""
+Modify percentage value to gain insight.
+e.g: In case greater chance of exception make the value .1 (if takes less time)
+			 lesser chance of exception make the value .9 (if and exceptio both take similar time)
+
+"""
+import timeit
+
+SETUP = """
+import random
+with open('/usr/share/dict/words', 'r') as fp:
+    words = [word.strip() for word in fp.readlines()]
+percentage = int(len(words) *.9)
+my_dict = dict([(w, w) for w in random.sample(words, percentage)])
+counter = 0
+"""
 
 LOOP_IF = """
-counter += 1
+word = random.choice(words)
+if word in my_dict:
+    counter += len(my_dict[word])
 """
 
 LOOP_EXCEPT = """
+word = random.choice(words)
 try:
-    counter += 1
-except:
+    counter += len(my_dict[word])
+except KeyError:
     pass
 """
 
 
 if __name__ == '__main__':
-    import timeit
     if_time = timeit.Timer(LOOP_IF, setup=SETUP)
     except_time = timeit.Timer(LOOP_EXCEPT, setup=SETUP)
-    print('using if statement: {}'.format(min(if_time.repeat(number=10 ** 7))))
-    print('using exception: {}'.format(min(except_time.repeat(number=10 ** 7))))
+    number = 1000000
+    min_if_time = min(if_time.repeat(number=number))
+    min_except_time = min(except_time.repeat(number=number))
+
+    print """using if statement:
+    minimum: {}
+    per_lookup: {}
+    """.format(min_if_time, min_if_time / number)
+
+    print """using exception:
+    minimum: {}
+    per_lookup: {}
+    """.format(min_except_time, min_except_time / number)
